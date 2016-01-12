7ae0871a9478d36379be188d9a832e90a894c4fc
Yichen Huang (Eugene) <yichen0831@gmail.com>
2016-01-12 13:43:44 +0800

    Corrected actor movement

--- core/src/com/ychstudio/actors/Player.java
+++ core/src/com/ychstudio/actors/Player.java
@@ -5,8 +5,6 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.ychstudio.gamesys.GM;
@@ -20,12 +18,6 @@ public class Player extends Actor {
     private float power = 0.8f;
     
     private static final float SQRT5 = (float) Math.sqrt(5.0);
-    
-    private ShapeRenderer shapeRenderer;
-    private float lxx = 0;
-    private float lyy = 0;
-    private float rxx = 0;
-    private float ryy = 0;
 
     public Player(Body body, Sprite sprite, float width, float height) {
         super(body, sprite, width, height);
@@ -36,27 +28,24 @@ public class Player extends Actor {
         right_throttle = false;
         
         rotation = 0;
-        
-        shapeRenderer = new ShapeRenderer();
     }
 
     @Override
     public void update(float delta) {
+    	// TODO remove this code
         if (Gdx.input.isKeyJustPressed(Input.Keys.SPACE)) {
             // reset player
             body.setLinearVelocity(0, 0);
             body.setTransform(10f, 2.8f, 0);
-            body.setGravityScale(0);
+            body.setAngularVelocity(0);
         }
         
         if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             left_throttle = true;
             float px = power * MathUtils.sin(-body.getAngle());
             float py = power * MathUtils.cos(-body.getAngle());
-            float dx = x - (width / 2f * SQRT5) * MathUtils.sin(-body.getAngle() + MathUtils.PI / 8f);
-            float dy = y - (height / 2f * SQRT5) * MathUtils.cos(-body.getAngle() + MathUtils.PI / 8f);
-            lxx = dx;
-            lyy = dy;
+            float dx = x - (width / 4f * SQRT5) * MathUtils.sin(-body.getAngle() + MathUtils.PI / 8f);
+            float dy = y - (height / 4f * SQRT5) * MathUtils.cos(-body.getAngle() + MathUtils.PI / 8f);
             
             body.applyLinearImpulse(px, py, dx, dy, true);
         }
@@ -68,10 +57,8 @@ public class Player extends Actor {
             right_throttle = true;
             float px = power * MathUtils.sin(-body.getAngle());
             float py = power * MathUtils.cos(-body.getAngle());
-            float dx = x - (width / 2f * SQRT5) * MathUtils.sin(-body.getAngle() - MathUtils.PI / 8f);
-            float dy = y - (height / 2f * SQRT5) * MathUtils.cos(-body.getAngle() - MathUtils.PI / 8f);
-            rxx = dx;
-            ryy = dy;
+            float dx = x - (width / 4f * SQRT5) * MathUtils.sin(-body.getAngle() - MathUtils.PI / 8f);
+            float dy = y - (height / 4f * SQRT5) * MathUtils.cos(-body.getAngle() - MathUtils.PI / 8f);
             
             body.applyLinearImpulse(px, py, dx, dy, true);
         }
@@ -96,22 +83,12 @@ public class Player extends Actor {
             flame.setOrigin(width / 2, height);
             flame.setRotation(rotation);
             flame.draw(batch);
-            shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
-            shapeRenderer.begin(ShapeType.Filled);
-            shapeRenderer.setColor(0.8f, 0.8f, 1, 1);
-            shapeRenderer.circle(lxx, lyy, 0.2f);
-            shapeRenderer.end();
         }
         if (right_throttle) {
             flame.setPosition(x, y - height);
             flame.setOrigin(0, height);
             flame.setRotation(rotation);
             flame.draw(batch);
-            shapeRenderer.setProjectionMatrix(batch.getProjectionMatrix());
-            shapeRenderer.begin(ShapeType.Filled);
-            shapeRenderer.setColor(1, 0.8f, 0.8f, 1);
-            shapeRenderer.circle(rxx, ryy, 0.2f);
-            shapeRenderer.end();
         }
     }
 
